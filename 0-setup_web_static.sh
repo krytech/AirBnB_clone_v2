ec2f17c4788b85f23a22a550d251616b77760d92
Carson Stearn <cstearn@gmail.com>
2021-08-26 16:18:26 +0000

    adding task 0, removing other files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,32 @@
 #!/usr/bin/env bash
-#Script to deploy static web pages
+# Sets up your web servers for the deployment of web_static
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-#install nginx, check for config
-if ! which nginx > /dev/null; then
-    apt-get update && apt-get install nginx -y
-   fi
-
-#create directories
-if [[ ! -e /data/web_static/releases/test]]; then
-    mkdir -p /data/web_static/releases/test
-fi
-
-if [[ ! -e /data/web_static/shared ]]; then
-    mkdir -p /data/web_static/shared
-    echo "<html>
-      <head>
-      </head>
-      <body>
-        Holberton School
-      </body>
-    </html>" > /data/web_static/releases/test/index.html
-fi
-
-#create symlink, removing if exists
-if [[ -e /data/web_static/current ]]; then
-    rm /data/web_static/current
-fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-#add /hbnb_static to default
-
-if ! grep -q "hbnb_static" /etc/nginx/sites-availabel/default; then
-  line='\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}'
-  sed -i "37i $line" /etc/nginx/sites-available/default
-fi
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
