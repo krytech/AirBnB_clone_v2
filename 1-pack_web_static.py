6f0d4f5e35cf7588abe699c245f24eb0289ce905
Carson Stearn <cstearn@gmail.com>
2021-08-26 17:54:58 +0000

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Generates a .tgz archive from the contents of the web_static.
+from fabric.api import local
+from datetime import date, datetime
+import os.path
+
+def do_pack():
+    """ Packs the web_static folder. """
+    date = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
