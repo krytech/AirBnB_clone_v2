f5d53a393d1e94557f87be215fdf514fed6c665a
Carson Stearn <cstearn@gmail.com>
2021-09-12 17:11:57 +0000

    all

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,12 +5,19 @@ Starts Flask web app
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Displays text"""
     return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays text"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
