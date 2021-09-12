0a7074e8f2f2dffdbddfa0ea926bfafc6c6bcd11
Carson Stearn <cstearn@gmail.com>
2021-09-12 19:14:08 +0000

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Starts Flask web app
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """Removes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/cities_by_state')
+def html_states():
+    """Displays html page for states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
